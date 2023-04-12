a61256f296740154638704ca80e219828a9a4289
Sohan Jain <snsohanjain@gmail.com>
2023-04-12 17:25:29 +0530

    ADDED-QUERY-OBJECT

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import { useData } from "./useData";
 import { Genre } from "./useGenre";
 
@@ -14,9 +15,9 @@ export interface Game {
     metacritic: number;
   }
   
-export const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
+export const useGames = (gameQuery : GameQuery) => 
 useData<Game>("/games", {
-  params: {genres: selectedGenre?.id 
-    , platforms: selectedPlatform?.id} } 
-    , [selectedGenre?.id, selectedPlatform?.id
+  params: {genres: gameQuery.genre?.id 
+    , platforms: gameQuery.platform?.id} } 
+    , [gameQuery
     ])
