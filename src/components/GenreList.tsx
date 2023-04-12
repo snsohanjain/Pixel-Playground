906ecbb702f0e9f75048a9aac1b7321d262cba8c
Sohan Jain <snsohanjain@gmail.com>
2023-04-12 15:57:47 +0530

    Update genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,8 +12,9 @@ import { getCroppedImageUrl } from "./image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -28,11 +29,12 @@ const GenreList = ({ onSelectGenre }: Props) => {
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize={"lg"}
               variant="link"
             >
-              {genre.name}
+              {genre.name.split(" ")[0]}
             </Button>
           </HStack>
         </ListItem>
